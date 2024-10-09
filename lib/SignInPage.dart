82b62f9f2e30bd4554c4208cc6dd7c4d32789c15
enigmayash <srivastavayashvardhan17@gmail.com>
2024-10-09 07:13:04 +0530

    SignUpPage done

---
+++ lib/SignInPage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+class SignInPage extends StatefulWidget{
+  @overrode
+  _SignInPageState createState => _SignInPageState();
+}
---
+++ lib/SignUpPage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+class SignUpPage extends StatefulWidget{
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+  }
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  Future<void> _signUp() async{
+    try{
+      Map<CognitoUserAttributeKey,String> userAttributes = {CognitoUserAttributeKey.email: _emailController.text};
+      SignUpResult result = await Amplify.Auth.signUp(
+        username: _emailController.text ,
+        password: _passwordController.text,
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      if(result.isSignUpComplete){
+        print("sign Up complete");
+      }
+      Navigator.pop(context);
+    }
+    catch (e){
+    print("sign up failed");
+  }
+  }
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Sign Up')),
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _signUp,
+            child: Text('Sign Up'),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+}
+
